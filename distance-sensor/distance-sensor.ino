5bfe5d935479f400338a849d315a810532b03120
Matthew Robertson <mattrobertson1414@gmail.com>
2019-10-29 13:03:41 -0600

    added distance sensor code

--- distance-sensor/distance-sensor.ino
+++ distance-sensor/distance-sensor.ino
@@ -1,12 +1,18 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
-#include "../config.h"
+#include <PubSubClient.h>
+#include "./config.h"
 
 ESP8266WiFiMulti wifiMulti;
+WiFiClient wifiClient;
+PubSubClient client("172.20.10.3", 1883, wifiClient);
+String device_name = "esp8266-distance-sensor";
 const char* ssid1 = HOME_WIFI_SSID;
 const char* passwd1 = HOME_WIFI_PASSWORD;
 const char* ssid2 = MATT_HOTSPOT_SSID;
 const char* passwd2 = MATT_HOTSPOT_PASSWORD;
+
+unsigned long timer_pointer = 0;
 
 void connectToWifi(){
   WiFi.mode(WIFI_STA);
@@ -31,11 +37,43 @@ void connectToWifi(){
   }
 }
 
+void reconnectToHub() {
+  while (!client.connected()){
+    if (client.connect((char*) device_name.c_str())){
+      Serial.println("Connected to MQTT Hub");
+      String message = "Distance sensor (" + device_name + ") connected @ " + WiFi.localIP().toString();
+      client.publish("/connections/distance", message.c_str());
+    } else {
+      Serial.println("Connection to MQTT failed, trying again...");
+      delay(3000);
+    }  
+  }  
+}
+
 void setup(){
   Serial.begin(115200);
   connectToWifi();
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
 void loop(){
+  if (!client.connected()){
+    reconnectToHub();
+  }
 
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+
+  client.loop();
+  if (millis() > timer_pointer + 1000){
+    timer_pointer = millis();
+    client.publish("/distance/p", String(distance));
+  }
 }
